f8e74f26d74a0f6814c2b12f796aa2f3ee3fb267
MorrisB-- <morris.ballenger@yahoo.com>
2016-06-20 20:57:33 -0500

    Added static variable to keep track of iterations

--- src/murfey/MakeChange.java
+++ src/murfey/MakeChange.java
@@ -3,26 +3,32 @@ package murfey;
 import java.util.Arrays;
 
 public class MakeChange {
+
+	static int count = 0;
+
 	public static void main(String[] args) {
 
 		System.out.println("Running...");
 
-		int[] coins = new int[30];
+		int[] coins = new int[10];
 		for (int i = 0; i < coins.length; i++)
-			coins[i] = (int) (Math.random() * 1000);
+			coins[i] = (int) (Math.random() * 200);
 
 		// Making sure a penny is in there
-		coins[0] = 1;
+		// coins[0] = 1;
 
 		int val = (int) (Math.random() * 100);
 		int mC = minCoins(val, coins);
 
 		System.out.println(Arrays.toString(coins));
 		System.out.println(mC + " coins needed for " + val);
-
+		System.out.println("Number of iterations " + count);
+		
 	}
 
 	public static int minCoins(int val, int[] coins) {
+		
+		count++;
 
 		if (val == 0)
 			return 0;
@@ -32,11 +38,18 @@ public class MakeChange {
 
 		for (int i = 0; i < coins.length; i++)
 			if (coins[i] <= val) {
+
 				c = minCoins(val - coins[i], coins);
+				if (c < 0)
+					continue;
 				c += 1;
 				if (c < minCoins)
 					minCoins = c;
 			}
+
+		// This is like a flag
+		if (minCoins > val)
+			return -1;
 
 		return minCoins;
 	}
