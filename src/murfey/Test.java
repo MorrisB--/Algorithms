1343cfd6b9a7ec425ccddd77cc8156ab38917f65
MorrisB-- <morris.ballenger@yahoo.com>
2016-06-20 19:49:42 -0500

    Added minCoins(int vval, int[] coins

--- src/murfey/Test.java
+++ src/murfey/Test.java
@@ -1,8 +1,12 @@
 package murfey;
 
 public class Test {
-	public static void main(String[] args){
-		
+	public static void main(String[] args) {
+
+	}
+
+	public static void minCoins(int val, int[] coins) {
+
 	}
 
 }
