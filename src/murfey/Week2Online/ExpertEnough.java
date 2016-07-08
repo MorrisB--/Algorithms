a9565b0fc29436933d3ec566835f9a0c1c7c93aa
MorrisB-- <morris.ballenger@yahoo.com>
2016-07-08 13:03:03 -0500

    Finished general structure, need to fix one bug

--- src/murfey/Week2Online/ExpertEnough.java
+++ src/murfey/Week2Online/ExpertEnough.java
@@ -5,27 +5,42 @@ import java.util.Scanner;
 public class ExpertEnough {
 	public static void main(String[] args) {
 
-		/*
-		 * 
-		 * 
-		 * Step 3:
-		 * 
-		 * Loop for queries Test query Output results
-		 */
 		Scanner keyboard = new Scanner(System.in);
 		int testCases = keyboard.nextInt();
 
 		// Going through each test case
 		for (int i = 0; i < testCases; i++) {
-			int databaseSize = keyboard.nextInt();
-
-			String[][] database = new String[databaseSize][3];
 
 			// Filling the database
+			int databaseSize = keyboard.nextInt();
+			String[][] database = new String[databaseSize][3];
 			for (int j = 0; j < databaseSize; j++) {
 				database[j][0] = keyboard.next();
 				database[j][1] = keyboard.next();
 				database[j][2] = keyboard.next();
+			}
+
+			// Running each query
+			int queries = keyboard.nextInt();
+			for (int k = 0; k < queries; k++) {
+
+				int queryPrice = keyboard.nextInt();
+
+				int count = 0;
+				int location = -1;
+				for (int l = 0; l < databaseSize; l++) {
+					if (queryPrice > Integer.parseInt(database[l][1])
+							&& queryPrice < Integer.parseInt(database[l][2])) {
+						count++;
+						location = l;
+					}
+				}
+
+				if (count == 1)
+					System.out.println(database[location][0]);
+				else
+					System.out.println("UNDETERMINED");
+
 			}
 		}
 
