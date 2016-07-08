e7f18f0b1fe5b0727ce5c3de818cdd04a979de8f
MorrisB-- <morris.ballenger@yahoo.com>
2016-07-08 10:52:26 -0500

    Added logic

--- src/murfey/Week2Online/ExpertEnough.java
+++ src/murfey/Week2Online/ExpertEnough.java
@@ -1,5 +1,24 @@
 package murfey.Week2Online;
 
 public class ExpertEnough {
+	public static void main(String[] args){
+		
+		/*
+		 * Step 1:
+		 * 
+		 * Loop for test cases
+		 * 
+		 * Step 2:
+		 * 
+		 * Loop for data
+		 * 		Insert data
+		 * 
+		 * Step 3:
+		 * 
+		 * Loop for queries
+		 * 		Test query
+		 * 		Output results
+		 */
+	}
 
 }
