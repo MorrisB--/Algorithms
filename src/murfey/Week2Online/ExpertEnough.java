53697c299a0fd4b02d829725a58c68af3ac8cfa1
MorrisB-- <morris.ballenger@yahoo.com>
2016-07-08 12:02:56 -0500

    Step 2 is complete

--- src/murfey/Week2Online/ExpertEnough.java
+++ src/murfey/Week2Online/ExpertEnough.java
@@ -3,27 +3,33 @@ package murfey.Week2Online;
 import java.util.Scanner;
 
 public class ExpertEnough {
-	public static void main(String[] args){
-		
+	public static void main(String[] args) {
+
 		/*
 		 * 
-		 * Step 2:
-		 * 
-		 * Loop for data
-		 * 		Insert data
 		 * 
 		 * Step 3:
 		 * 
-		 * Loop for queries
-		 * 		Test query
-		 * 		Output results
+		 * Loop for queries Test query Output results
 		 */
 		Scanner keyboard = new Scanner(System.in);
 		int testCases = keyboard.nextInt();
-		
-		for (int i = 0; i < testCases; i++){
-			
+
+		// Going through each test case
+		for (int i = 0; i < testCases; i++) {
+			int databaseSize = keyboard.nextInt();
+
+			String[][] database = new String[databaseSize][3];
+
+			// Filling the database
+			for (int j = 0; j < databaseSize; j++) {
+				database[j][0] = keyboard.next();
+				database[j][1] = keyboard.next();
+				database[j][2] = keyboard.next();
+			}
 		}
+
+		keyboard.close();
 	}
 
 }
