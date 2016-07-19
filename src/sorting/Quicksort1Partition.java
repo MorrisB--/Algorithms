85a05bf203f0fe7b6edcff0b6daaa760806a0747
MorrisB-- <morris.ballenger@yahoo.com>
2016-07-18 20:22:50 -0500

    Started Quicksort1Partition

---
+++ src/sorting/Quicksort1Partition.java
@@ -0,0 +1,25 @@
+package sorting;
+
+import java.util.Scanner;
+
+public class Quicksort1Partition {
+
+	public static void main(String[] args) {
+
+		Scanner keyboard = new Scanner(System.in);
+		int[] array = new int[keyboard.nextInt()];
+
+		for (int i = 0; i < array.length; i++)
+			array[i] = keyboard.nextInt();
+
+		int[] left = new int[0], right = new int[0];
+		
+		for(int i =1;i<array.length;i++){
+			if(array[i]> array[0]){
+				
+			}
+		}
+
+		keyboard.close();
+	}
+}
