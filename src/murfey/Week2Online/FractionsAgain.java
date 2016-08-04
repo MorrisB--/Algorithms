0970545c5c4160b90338cee63cc67020667424ae
MorrisB-- <morris.ballenger@yahoo.com>
2016-08-04 09:37:07 -0500

    Created files

---
+++ src/murfey/Week2Online/FractionsAgain.java
@@ -0,0 +1,17 @@
+package murfey.Week2Online;
+
+import java.util.Scanner;
+
+public class FractionsAgain {
+	public static void main(String[] args) {
+
+		Scanner keyboard = new Scanner(System.in);
+		int testCases = keyboard.nextInt();
+
+		for (int i = 0; i < testCases; i++) {
+
+		}
+		keyboard.close();
+	}
+
+}
